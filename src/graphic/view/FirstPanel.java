73a217bb350666d3952571e8e519677dee2d64f7
juvekeeper41 <juvekeeper41@gmail.com>
2016-11-01 09:28:03 -0600

    Created the layout for the panel.

--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -1,16 +1,59 @@
 package graphic.view;
 
+import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import graphic.controller.FirstGUIController;
+import javax.swing.SpringLayout;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstPanel extends JPanel
 {
 	private FirstGUIController baseController;
-	
+	private JButton colorButton;
+	private JButton randomButton;
+	private SpringLayout baseLayout;
+
 	public FirstPanel(FirstGUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+
+		colorButton = new JButton("Color!");
+		randomButton = new JButton("Random");
+		baseLayout= new SpringLayout();
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+
+	}
+
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.CYAN);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, colorButton, -10, SpringLayout.EAST, this);
+	}
+
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.MAGENTA);
+			}
+		});
 	}
 }
